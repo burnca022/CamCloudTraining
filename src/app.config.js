6ee7b98ab4d408ff5f0aa407a5811ce34dcb5b3e
burnca022 <burnca02@gmail.com>
2022-11-03 16:11:08 +0000

    Initial commit

---
+++ src/app.config.js
@@ -0,0 +1,10 @@
+/**
+ * Place to store data specific to this instance
+ * DO NOT store secrets here
+ * DO NOT store state here - must be part of a stateless service
+ */
+class AppConfig {
+    team = 'Team 2 - Cam Burns';
+    version = '1.0.1';
+}
+module.exports = AppConfig;
